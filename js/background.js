ae299775ad2e2fbdb67f5f2ab491569eb9fc3e49
Vladimir Mitrović <v@brutalism.rs>
2017-10-14 01:41:22 +0200

    Blobby Hundreds

--- js/background.js
+++ js/background.js
@@ -1,6 +1,19 @@
+var mainColor = new THREE.Color(0x3A51F0);
+var backgroundColor = new THREE.Color(0xFFFFFF);
+var timeScale = 0.2;
+var distortionIntensity = new THREE.Vector3(50.0, 10.0, 0.0);
+var sphereCount = 12;
+var noiseFrequencyMin = 0.01;
+var noiseFrequencyMax = 0.06;
+var positionXVariance = 50.0;
+var positionYVariance = 20.0;
+var radiusMin = 20.0;
+var radiusMax = 40.0;
+var segments = 256;
+var rings = 128;
+
 var renderer, scene, camera;
-var sphere, uniforms, shaderMaterial;
-var displacement, noise;
+var allUniforms = [];
 
 init();
 var startTime = Date.now();
@@ -11,43 +24,45 @@ function init() {
 	camera.position.z = 300;
 
 	scene = new THREE.Scene();
-	scene.background = new THREE.Color(0x050505);
+	scene.background = backgroundColor;
 
-	uniforms = {
-		amplitude: {
-			value: 1.0
-		},
-		time: {
-			value: 0.5
-		},
-		color: {
-			value: new THREE.Color(0x00A5A5)
-		}
-	};
+	for (var i = 0; i < sphereCount; i++) {
+		var currentUniforms = {
+			time: {
+				value: 0.0,
+				type: "f"
+			},
+			timeOffset: {
+				value: randRange(0.0, 10.0)
+			},
+			noiseFrequency: {
+				value: randRange(noiseFrequencyMin, noiseFrequencyMax)
+			},
+			distortionIntensity: {
+				value: distortionIntensity,
+				type: "v3"
+			},
+			color: {
+				value: mainColor
+			}
+		};
 
-	shaderMaterial = new THREE.ShaderMaterial({
-		uniforms: uniforms,
-		vertexShader: document.getElementById('vertexshader').textContent,
-		fragmentShader: document.getElementById('fragmentshader').textContent
-	});
+		allUniforms.push(currentUniforms);
+		var material = new THREE.ShaderMaterial({
+			uniforms: currentUniforms,
+			vertexShader: document.getElementById('vertexshader').textContent,
+			fragmentShader: document.getElementById('fragmentshader').textContent
+		});
 
-	var radius = 50,
-		segments = 128,
-		rings = 64;
+		var radius = randRange(radiusMin, radiusMax);
+		var geometry = new THREE.SphereBufferGeometry(radius, segments, rings);
 
-	var geometry = new THREE.SphereBufferGeometry(radius, segments, rings);
+		var sphere = new THREE.Mesh(geometry, material);
+		sphere.translateX(randNorm(positionXVariance));
+		sphere.translateY(randNorm(positionYVariance));
 
-	displacement = new Float32Array(geometry.attributes.position.count);
-	noise = new Float32Array(geometry.attributes.position.count);
-
-	for (var i = 0; i < displacement.length; i++) {
-		noise[i] = Math.random() * 5;
+		scene.add(sphere);
 	}
-
-	geometry.addAttribute('displacement', new THREE.BufferAttribute(displacement, 1));
-
-	sphere = new THREE.Mesh(geometry, shaderMaterial);
-	scene.add(sphere);
 
 	renderer = new THREE.WebGLRenderer();
 	renderer.setPixelRatio(window.devicePixelRatio);
@@ -59,6 +74,18 @@ function init() {
 	window.addEventListener('resize', onWindowResize, false);
 }
 
+function randNorm(radius) {
+    var u = 0, v = 0;
+    while(u === 0) u = Math.random(); //Converting [0,1) to (0,1)
+    while(v === 0) v = Math.random();
+    return radius * Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
+}
+
+function randRange(min, max) {
+	var r=min + Math.random() * (max - min);
+	return r;
+}
+
 function onWindowResize() {
 	camera.aspect = window.innerWidth / window.innerHeight;
 	camera.updateProjectionMatrix();
@@ -68,7 +95,6 @@ function onWindowResize() {
 
 function animate() {
 	requestAnimationFrame(animate);
-
 	render();
 }
 
@@ -76,20 +102,9 @@ function render() {
 	var elapsedMilliseconds = Date.now() - startTime;
 	var time = elapsedMilliseconds / 1000.;
 
-	uniforms.time.value = time;
-	uniforms.amplitude.value = 2.5 * Math.sin(sphere.rotation.y * 0.125);
-	// uniforms.color.value.offsetHSL(0.0005, 0, 0);
-
-	for (var i = 0; i < displacement.length; i++) {
-		displacement[i] = Math.sin(0.1 * i + time);
-
-		noise[i] += 0.5 * (0.5 - Math.random());
-		noise[i] = THREE.Math.clamp(noise[i], -5, 5);
-
-		displacement[i] += noise[i];
+	for (uniform of allUniforms) {
+		uniform.time.value = time * timeScale;
 	}
-
-	sphere.geometry.attributes.displacement.needsUpdate = true;
 
 	renderer.render(scene, camera);
 }
